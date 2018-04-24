d11dce3ac4d61c4d83f8df685648a3f36ba9bb35
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-24 14:52:05 -0400

    Caramel Template 1.0  /main.js

--- main.js
+++ main.js
@@ -171,7 +171,7 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'datastore', 'vu
                     });
                     await Promise.all([this.$store.dispatch("getData", "property")]);
                     // avoid making LOAD_META_DATA call for now as it will cause the entire Promise.all to fail since no meta data is set up.
-                    let results = await Promise.all([this.$store.dispatch("INITIALIZE_LOCALE"), this.$store.dispatch("getData", "hours"), this.$store.dispatch("getData", "stores"), this.$store.dispatch("getData", "repos"), this.$store.dispatch("LOAD_META_DATA")]);
+                    let results = await Promise.all([this.$store.dispatch("INITIALIZE_LOCALE"), this.$store.dispatch("getData", "hours"), this.$store.dispatch("getData", "stores"), this.$store.dispatch("getData", "repos")]);
                     // this.$store.dispatch("LOAD_META_DATA")
                 } catch (e) {
                     console.log("Error loading data: " + e.message);
