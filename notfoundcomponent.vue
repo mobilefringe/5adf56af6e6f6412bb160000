2b4cbd18e7ca6c9a2a356c6c49508f2a4d82f2b4
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-26 16:02:31 -0400

    Caramel Template 1.0  /notfoundcomponent.vue

---
+++ notfoundcomponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="row main_container">
+    <div class="large-6 columns">
+      <h1>{{text}}</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+    define(["Vue"], function(Vue) {
+        return Vue.component("not-found-component", {
+            template: template, // the variable template will be injected
+            data: function() {
+                return {
+                    "text": "Custom 404 Page"
+                };
+            }
+        });
+    });
+</script>
