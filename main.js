b00a904d724c9bcc9422cbf05b1ffd4f39d3b333
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-27 15:07:10 -0400

    Caramel Template 1.0  /main.js

--- main.js
+++ main.js
@@ -166,7 +166,7 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'datastore', 'vu
             loadData: async function() {
                 try {
                     await this.$store.dispatch('initializeApi', {
-                        site: "canyoncrest",
+                        site: "caramel",
                         version: "v4"
                     });
                     await Promise.all([this.$store.dispatch("getData", "property")]);
