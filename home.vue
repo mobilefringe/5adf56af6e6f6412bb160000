8087e9c52a5d27196936640870d4546d119c3dbc
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-30 15:51:50 -0400

    Caramel Template 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -28,6 +28,7 @@
                                     </div>
                                     <div class="teamy__back"></div>
                                     <p v-if="locale=='en-ca'" class="feature_text">{{item.name}}</p>
+                                    <p v-else class="feature_text">{{item.name_2}}</p>
                                 </router-link>
                             </div>
                         </article>
